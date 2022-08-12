1c0c574c2293bf18770945e2d836a58eaeeefcb8
Constantine_Mukhin <mol.Kostya@mail.ru>
2022-08-13 00:32:36 +0500

    Symfony 6.1.3 was added

--- nginx/scripts/run_nginx.sh
+++ nginx/scripts/run_nginx.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export DOLLAR='$'
 envsubst < /etc/nginx/conf.d/pitch.conf > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
-cat /etc/nginx/nginx.conf
+# cat /etc/nginx/nginx.conf
